f0c99f853e62455a9c4cc879af691d199714d0d4
HOANG THE ANH <therealhoangtheanh@gmail.com>
2023-11-04 20:31:12 +0700

    [Auth] Login Panel Initialization

---
+++ src/pages/auth/components/login-panel.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+const LogInPanel = (): JSX.Element => {
+  return (
+    <div className="card card-compact w-96 bg-base-100 shadow-xl p-4">
+      <div className="grid grid-cols-1">
+        <button className="btn text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mb-2">
+          <svg
+            className="w-4 h-4 mr-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 18 19"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          Sign in with Google
+        </button>
+        <button className="btn text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mb-2">
+          <svg
+            className="w-4 h-4 mr-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 8 19"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          Sign in with Facebook
+        </button>
+        <div className="flex flex-row self-center">
+          <hr className="border-t-2 w-5/12 divide-solid self-center"></hr>
+          <text className="w-2/12 text-center">OR</text>
+          <hr className="border-t-2 w-5/12 divide-solid self-center"></hr>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <a className="link link-primary">Forgot password?</a>
+        <button className="btn btn-primary">Log In</button>
+        <p>
+          Haven't got an account?{" "}
+          <a className="link link-primary">Sign up here!</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LogInPanel;
---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogInPanel from './components/login-panel';
+
+const AuthPage = (): JSX.Element => {
+    return (
+        <div>
+            <LogInPanel></LogInPanel>
+        </div>
+    )
+}
+
+export default AuthPage;
