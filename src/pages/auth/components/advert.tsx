55596ff65d8f7d6ee7ca0bb5856de99e20be8eff
HOANG THE ANH <therealhoangtheanh@gmail.com>
2023-11-11 14:09:42 +0700

    Create advertisement and initialize auth page

---
+++ src/pages/auth/components/advert.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Advert = (): JSX.Element => {
+  return (
+    <main>
+      <text className="w-full flex justify-center p-4 text-3xl">
+        Welcome to{" "}
+        <text className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text font-bold">
+          Zhoot
+        </text>
+        !
+      </text>
+      <div className="mockup-browser border bg-base-300">
+        <div className="mockup-browser-toolbar">
+          <div className="input">https://zhoot.apcs</div>
+        </div>
+        <div className="">
+          <iframe
+            src="https://www.youtube.com/embed/7XzfWHdDS9Q?si=V_WkdXhX0idMFLWB"
+            frameBorder="0"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+            title="video"
+            className="w-full h-[700px]"
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Advert;
--- src/pages/auth/components/login-panel.tsx
+++ src/pages/auth/components/login-panel.tsx
@@ -2,10 +2,6 @@ import React from "react";
 
 const LogInPanel = (): JSX.Element => {
 
-  function abcxyz() {
-
-  }
-
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl p-4">
       <div className="grid grid-cols-1">
@@ -25,7 +21,7 @@ const LogInPanel = (): JSX.Element => {
           </svg>
           Sign in with Google
         </button>
-        <button onClick={abcxyz} className="btn text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mb-2">
+        <button className="btn text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mb-2">
           <svg
             className="w-4 h-4 mr-2"
             aria-hidden="true"
@@ -46,29 +42,29 @@ const LogInPanel = (): JSX.Element => {
           <text className="w-2/12 text-center">OR</text>
           <hr className="border-t-2 w-5/12 divide-solid self-center"></hr>
         </div>
-        <div className="form-control w-full max-w-xs">
+        <div className="form-control w-full">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
           <input
             type="text"
             placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
+            className="input input-bordered w-full"
           />
         </div>
-        <div className="form-control w-full max-w-xs">
+        <div className="form-control w-full">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
           <input
             type="password"
             placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
+            className="input input-bordered w-full"
           />
         </div>
-        <a className="link link-primary">Forgot password?</a>
+        <a className="link link-primary flex justify-center p-2">Forgot password?</a>
         <button className="btn btn-primary">Log In</button>
-        <p>
+        <p className="flex justify-center p-2">
           Haven't got an account?{" "}
           <a className="link link-primary">Sign up here!</a>
         </p>
